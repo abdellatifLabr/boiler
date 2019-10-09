6fa23fdcb340daf116660815ae2c2bab52a2f672
labTifo <tifo.zelo@gmail.com>
2019-10-09 13:41:12 +0200

    move * to lib

---
+++ bin/app.js
@@ -0,0 +1,130 @@
+const path = require('path');
+const fs = require('fs');
+const clc = require('cli-color');
+const cli = require('../lib/cli');
+const cmd = require('../lib/cmd');
+const progressBar = require('../lib/progress-bar');
+const { render, createDirIfNotExists } = require('../lib/utils');
+
+/* Arguments parsing */
+const projectDir = cli.directory;
+const entries = cli.entries || ['main'];
+const scripting = (cli.js) ? 'js' : 'ts';
+const templating = (cli.html) ? 'html' : 'pug';
+const styling = (cli.css) ? 'css' : 'scss';
+const purgeCss = cli.purgeCss;
+const devServer = cli.devServer;
+const git = cli.git;
+const context = { projectDir, entries, scripting, styling, templating, purgeCss, devServer };
+
+/* Creating project structure */
+if (fs.existsSync(projectDir)) {
+    console.log('Project directory already exists.');
+} else {
+    if (projectDir != '.') {
+        fs.mkdirSync(projectDir);
+    }
+    fs.mkdirSync(path.join(projectDir, 'src'));
+    fs.mkdirSync(path.join(projectDir, 'src', 'assets'));
+    fs.mkdirSync(path.join(projectDir, 'dist'));
+}
+
+/* Write config file */
+const wpConfigDir = path.join(projectDir, 'webpack.config.js');
+console.log(clc.green('CREATE'), wpConfigDir);
+fs.writeFileSync(
+    wpConfigDir, 
+    render('../lib/templates/webpack.config.js.hbs', context)
+);
+
+/* Create entry module */
+createDirIfNotExists(path.join(projectDir, `src/${scripting}`));
+for (let entrie of entries) {
+    const script = path.join(projectDir, `src/${scripting}/${entrie}.${scripting}`);
+    console.log(clc.green('CREATE'), script);
+    fs.writeFileSync(
+        script,
+        render('../lib/templates/script.hbs', { ...context, entrie })
+    );
+}
+if (scripting == 'ts') {
+    /* Generate tsconfig.json file */
+    const tsConfigDir = path.join(projectDir, 'tsconfig.json');
+    console.log(clc.green('CREATE'), tsConfigDir);
+    fs.writeFileSync(
+        tsConfigDir,
+        render('../lib/templates/tsconfig.json.hbs', {})
+    );
+}
+
+/* Create styles file */
+createDirIfNotExists(path.join(projectDir, `src/${styling}`));
+const stylesDir = path.join(projectDir, `src/${styling}/styles.${styling}`);
+console.log(clc.green('CREATE'), stylesDir);
+fs.writeFileSync(
+    stylesDir,
+    '/* Main Styles File */'
+);
+
+/* Create tempaltes files */
+const templatesDir = path.join(projectDir, `src/index.${templating}`);
+console.log(clc.green('CREATE'), templatesDir);
+fs.writeFileSync(
+    templatesDir, 
+    render('../lib/templates/index.hbs', context)    
+);
+
+/* Write package.json file */
+const pkgJsonDir = path.join(projectDir, 'package.json');
+console.log(clc.green('CREATE'), pkgJsonDir);
+fs.writeFileSync(
+    pkgJsonDir,
+    render('../lib/templates/package.json.hbs', context)
+);
+
+/* Filtering & installing dependencies */
+const dependencies = [
+    'copy-webpack-plugin',
+    'css-loader',
+    'html-webpack-plugin',
+    'pug-html-loader',
+    'node-sass',
+    'sass-loader',
+    'style-loader',
+    '@fullhuman/purgecss-loader',
+    'ts-loader',
+    'webpack',
+    'webpack-dev-server',
+    'webpack-cli',
+    'typescript'
+].filter(dep => {
+    if (styling == 'css' && (dep == 'sass-loader' || dep == 'node-sass')) return false;
+    if (!purgeCss && dep == '@fullhuman/purgecss-loader') return false;
+    if (templating == 'html' && dep == 'pug-html-loader') return false;
+    if (scripting == 'js' && dep == 'ts-loader') return false;
+    if (scripting == 'js' && dep == 'typescript') return false;
+    if (!devServer && dep == 'webpack-dev-server') return false;
+    return true;
+});
+console.log(dependencies);
+process.exit()
+(async function () {
+    process.chdir(projectDir);
+    console.log('> npm install');
+    progressBar.start(dependencies.length, 0);
+    for (let [i, dep] of dependencies.entries()) {
+        await cmd(`npm install -D ${dep}`);
+        progressBar.update(i+1);
+    }
+    progressBar.stop();
+    if (git) {
+        console.log('> git init');
+        await cmd('git init');
+        fs.copyFileSync('../lib/templates/.gitignore', '.gitignore');
+        fs.writeFileSync('src/assets/.gitkeep', '');
+    }
+})();
+
+/*
+    * bin files https://medium.com/@thatisuday/creating-cli-executable-global-npm-module-5ef734febe32
+*/
